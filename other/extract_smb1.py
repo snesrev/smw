148c0f897019798c21399631c6b012e75a25c83c
Snesrev <snesrev@protonmail.com>
2023-06-01 17:53:05 +0200

    Verify the smas hash

--- other/extract_smb1.py
+++ other/extract_smb1.py
@@ -1,6 +1,13 @@
 # pip install zstandard
 import zstandard
-dict_data = zstandard.ZstdCompressionDict(open('smas.sfc', 'rb').read())
+import hashlib
+
+SHA1_HASH = 'c05817c5b7df2fbfe631563e0b37237156a8f6b6'
+smas = open('smas.sfc', 'rb').read()
+hash = hashlib.sha1(smas).hexdigest()
+if hash != SHA1_HASH:
+  raise Exception('You need SMAS with sha1 ' + SHA1_HASH + ' yours is ' + hash)
+dict_data = zstandard.ZstdCompressionDict(smas)
 cctx = zstandard.ZstdDecompressor(dict_data=dict_data)
 with open('smb1.zst', 'rb') as ifp:
     with open('smb1.sfc', 'wb') as ofp:
